6afbbd61d0360d82fee0af79e1e724a79f2125d5
Kpiiale <yuliana.capito@udla.edu.ec>
2024-10-18 08:55:40 -0500

    2

--- Taller-YulianaCapito/Models/Equipo.cs
+++ Taller-YulianaCapito/Models/Equipo.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Taller_YulianaCapito.Models
 {
@@ -8,7 +9,12 @@ namespace Taller_YulianaCapito.Models
         public int Id { get; set; }
         [Required]
         public string Nombre { get; set; }
+        [MaxLength(100)]
         public string Ciudad { get; set; }
         public int Titulos { get; set; }
         public string AceptaExtranjeros {  get; set; }
+        public Jugadores Jugadores { get; set; } 
+
+        [ForeignKey("Jugadores")]
+        public int IdJugador { get; set; }
 }
--- Taller-YulianaCapito/Models/Estadio.cs
+++ Taller-YulianaCapito/Models/Estadio.cs
@@ -1,7 +1,20 @@
-﻿namespace Taller_YulianaCapito.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taller_YulianaCapito.Models
 {
     public class Estadio
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Direccion { get; set; }
+        [MaxLength(1000)]
+        public string Ciudad { get; set; }
+        [Range (0,50000)]
+        public int Capacidad { get; set; }
+        public Equipo Equipo { get; set; }
+        public int EquipoId { get; set;
+
 
     }
 }
